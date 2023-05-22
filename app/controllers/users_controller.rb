55aa2bacef36cef4fe646038c1669a170b1dfb55
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-05-22 16:51:53 -0400

    create login actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,17 +11,36 @@ class UsersController < ApplicationController
 
   def create
     @user = User.create(user_params)
-    if @user.save
-      redirect_to user_path(@user)
-    else 
+    if params[:password] == params[:password_confirmation]
+      if @user.save 
+        redirect_to user_path(@user)
+      else 
+        redirect_to "/register"
+        flash[:alert] = "Please fill in all fields. Email must be unique."
+      end
+    else
       redirect_to "/register"
-      flash[:alert] = "Please fill in all fields. Email must be unique."
+      flash[:alert] = "Passwords do not match."
+    end
+  end
+
+  def login_form
+  end
+
+  def login_user
+    user = User.find_by(email: params[:email])
+    if user.authenticate(params[:password])
+      redirect_to user_path(user)
+      flash[:notice] = "Welcome, #{user.name}! You are now logged in."
+    else
+      redirect_to "/login"
+      flash[:alert] = "Incorrect email/password combination."
     end
   end
 
   private
 
   def user_params
-    params.permit(:name, :email)
+    params.permit(:name, :email, :password, :password_confirmation)
   end
 end
